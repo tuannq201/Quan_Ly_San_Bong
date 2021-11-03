5ac259788643019c32b870724670bcd86560897f
Luong Nguyen <protate3008@gmail.com>
2021-11-03 12:13:17 +0700

    Thêm entity.

--- app/src/main/java/com/example/myapplication/entity/PhieuThue.java
+++ app/src/main/java/com/example/myapplication/entity/PhieuThue.java
@@ -7,18 +7,16 @@ import java.util.Date;
 public class PhieuThue {
     public int maPT,maSan,maNT,caThue,tienSan;
     public Date ngayThue;
-    public Bitmap anhSan;
 
     public PhieuThue() {
     }
 
-    public PhieuThue(int maPT, int maSan, int maNT, int caThue, int tienSan, Date ngayThue, Bitmap anhSan) {
+    public PhieuThue(int maPT, int maSan, int maNT, int caThue, int tienSan, Date ngayThue) {
         this.maPT = maPT;
         this.maSan = maSan;
         this.maNT = maNT;
         this.caThue = caThue;
         this.tienSan = tienSan;
         this.ngayThue = ngayThue;
-        this.anhSan = anhSan;
     }
 }
