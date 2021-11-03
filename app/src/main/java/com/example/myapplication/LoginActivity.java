315c82a8a18f224ca5e9da12f76091843f6f6cf6
admin <tuannq201@gmail.com>
2021-11-03 12:17:58 +0700

    tuan push 12:17

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,12 +1,31 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Dialog dialog;
+    Button btn_register, btn_camera, btn_album, btn_save;
+    ImageView iv_camera_result;
+    public static final int REQUEST_CODE_CAMERA = 0;
+    public static final int REQUEST_CODE_FOLDER = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +37,50 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void openDialog(View v){
 
+
+    public void openDialog(View v){
+        dialog = new Dialog(LoginActivity.this);
+        dialog.setContentView(R.layout.dialog_register_user);
+        btn_camera = dialog.findViewById(R.id.btn_camera);
+        btn_album = dialog.findViewById(R.id.btn_albuml);
+        iv_camera_result = dialog.findViewById(R.id.iv_image_register);
+
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        });
+        btn_album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_FOLDER);
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            iv_camera_result.setImageBitmap(bitmap);
+        }
+        if (requestCode == REQUEST_CODE_FOLDER && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                iv_camera_result.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
