361dd33e789b7d84e4d76d2b2f3e7dbe505d22c7
ngan <phucngan301@gmail.com>
2021-11-04 09:22:15 +0700

    Layout Lich Sân

--- app/src/main/java/com/example/myapplication/UI/LichSanFragment.java
+++ app/src/main/java/com/example/myapplication/UI/LichSanFragment.java
@@ -12,10 +12,10 @@ import com.example.myapplication.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ThongTinSanFragment#newInstance} factory method to
+ * Use the {@link LichSanFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ThongTinSanFragment extends Fragment {
+public class LichSanFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class ThongTinSanFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ThongTinSanFragment() {
+    public LichSanFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class ThongTinSanFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ThongTinSanFragment.
+     * @return A new instance of fragment LichSanFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ThongTinSanFragment newInstance(String param1, String param2) {
-        ThongTinSanFragment fragment = new ThongTinSanFragment();
+    public static LichSanFragment newInstance(String param1, String param2) {
+        LichSanFragment fragment = new LichSanFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class ThongTinSanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thong_tin_san, container, false);
+        return inflater.inflate(R.layout.fragment_lich_san, container, false);
     }
 }
